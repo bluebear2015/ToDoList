8989d56f9ca74b04b0a86df5a276b04ebef4c4a1
bluebear2015 <samluna61091@gmail.com>
2023-09-05 07:21:19 -0600

    vngv

---
+++ app/AppState.js
@@ -0,0 +1,48 @@
+import { Value } from './models/Value.js'
+import { EventEmitter } from './utils/EventEmitter.js'
+import { isValidProp } from './utils/IsValidProp.js'
+import { loadState } from './utils/Store.js'
+import { Note } from './models/Note.js'
+import { Quote } from './models/Quote.js'
+
+class ObservableAppState extends EventEmitter {
+  page = ''
+  user = null
+  /** @type {import('./models/Account.js').Account | null} */
+  // @ts-ignore
+  account = null
+  /** @type {import('./models/Value.js').Value[]} */
+  values = loadState('values', [Value])
+  socketData = []
+
+
+  /** @type {import('./models/Note.js').Note[]} */
+  notes = []
+
+  /** @type {import('./models/Weather.js').Weather | null} */
+  weather = null
+  showF = null
+
+  pictures = null
+
+  /** @type {import('./models/Quote.js').Quote[]} */
+  quotes = []
+
+  // Used to load initial data
+  init() {
+
+  }
+}
+
+export const AppState = new Proxy(new ObservableAppState(), {
+  get(target, prop) {
+    isValidProp(target, prop)
+    return target[prop]
+  },
+  set(target, prop, value) {
+    isValidProp(target, prop)
+    target[prop] = value
+    target.emit(prop, value)
+    return true
+  }
+})
